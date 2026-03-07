519515c625e0d7f36302a06a1b7cec7cfb99bf03
Yaritza <eyaritza90@gmail.com>
2026-03-07 14:37:07 -0700

    Agregar index.php para probar la clase Usuario

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -2,11 +2,8 @@
 
 require_once "Usuario.php";
 
+$usuario = new Usuario("Yaritza Laureano", "yaritza@gmail.com");
 
-$usuario = new Usuario("Tu Nombre", "correo@ejemplo.com");
-
-
-echo "<h2>Datos del Usuario</h2>";
 echo "Nombre: " . $usuario->getNombre() . "<br>";
 echo "Correo: " . $usuario->getCorreo();
 
