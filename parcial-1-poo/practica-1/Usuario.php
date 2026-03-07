8b6a8c60b299e5c5b18f2df2105ece49a4f7fcbb
Yaritza <eyaritza90@gmail.com>
2026-03-07 14:36:54 -0700

    Agregar clase Usuario con atributos privados y constructor

--- parcial-1-poo/practica-1/Usuario.php
+++ parcial-1-poo/practica-1/Usuario.php
@@ -1,30 +1,32 @@
 <?php
 // Práctica 1 - POO en PHP
+
 class Usuario {
 
-    private string $nombre;
-    private string $correo;
+    private $nombre;
+    private $correo;
 
-    public function __construct(string $nombre, string $correo) {
+    public function __construct($nombre, $correo) {
         $this->nombre = $nombre;
         $this->correo = $correo;
     }
 
-    public function getNombre(): string {
+    public function getNombre() {
         return $this->nombre;
     }
 
-    public function getCorreo(): string {
+    public function getCorreo() {
         return $this->correo;
     }
 
-    public function setNombre(string $nombre): void {
+    public function setNombre($nombre) {
         $this->nombre = $nombre;
     }
 
-    public function setCorreo(string $correo): void {
-        $this->correo = $correo;
+    public function setCorreo($correo) {
+       
+    $this->correo = $correo;
     }
-}
 
+}
 ?>
