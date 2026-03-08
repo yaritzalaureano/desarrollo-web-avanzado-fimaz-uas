c03e8dc590e02cd3456e452c7e37af0e3074723c
Yaritza <eyaritza90@gmail.com>
2026-03-08 01:27:24 -0700

    Agregar clase Usuario práctica 3

--- parcial-1-poo/practica-3/clases/Usuario.php
+++ parcial-1-poo/practica-3/clases/Usuario.php
@@ -1,1 +1,24 @@
 
+<?php
+class Usuario {
+    private $nombre;
+    private $correo;
+
+    public function __construct($nombre, $correo){
+        $this->nombre = $nombre;
+        // Validar correo
+        if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Correo inválido: $correo");
+        }
+        $this->correo = $correo;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function getCorreo(){
+        return $this->correo;
+    }
+}
+?>
