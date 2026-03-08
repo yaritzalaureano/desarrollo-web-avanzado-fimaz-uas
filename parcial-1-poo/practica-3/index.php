af4ab758037e1d40df99d291d30d0f092700f99f
Yaritza <eyaritza90@gmail.com>
2026-03-08 02:13:31 -0700

    Agregar index práctica 3

--- parcial-1-poo/practica-3/index.php
+++ parcial-1-poo/practica-3/index.php
@@ -1,1 +1,27 @@
+<?php
+require_once "clases/Usuario.php";
+require_once "clases/Admin.php";
+require_once "clases/Alumno.php";
+
+try {
+    $admin = new Admin("Alfonso Calderon", "alfonsoncal@gmail.com");
+    echo "Nombre: " . $admin->getNombre() . "<br>";
+    echo "Correo: " . $admin->getCorreo() . "<br>";
+    echo "Rol: " . $admin->getRol() . "<br><br>";
+
+    $alumno = new Alumno("Yaritza Laureano", "yaritza@gmail.com", "2023001");
+    echo "Nombre: " . $alumno->getNombre() . "<br>";
+    echo "Correo: " . $alumno->getCorreo() . "<br>";
+    echo "Rol: " . $alumno->getRol() . "<br>";
+    echo "Matrícula: " . $alumno->getMatricula() . "<br><br>";
+
+    
+    $usuarioInvalido = new Usuario("Alejandra Cruz",
+"correo-invalido");
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
+
+
 
