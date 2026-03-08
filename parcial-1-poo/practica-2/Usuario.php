2c080b1bcfd57862bd6bb7cca46a3095ce50833a
Yaritza <eyaritza90@gmail.com>
2026-03-07 18:14:36 -0700

    Agregar clase Usuario para práctica 2

---
+++ parcial-1-poo/practica-2/Usuario.php
@@ -0,0 +1,27 @@
+<?php
+class Usuario {
+    private $nombre;
+    private $correo;
+
+    public function __construct($nombre, $correo) {
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getCorreo() {
+        return $this->correo;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function setCorreo($correo) {
+        $this->correo = $correo;
+    }
+}
+?>
